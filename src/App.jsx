5a2e2751ccdd54525c1b6fe8d4589a79c56efb3f
AlbertoLcm <alberto.cm4007@gmail.com>
2023-03-19 21:15:19 -0600

    About added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
 import './App.css'
 import styles from './styles/Home.module.css'
 import Header from './components/Header'
-import image from './assets/images/test.png'
+import image from './assets/images/me.jpg'
+import svg from './assets/images/Untitled.svg'
 
 function App() {
   return (
     <div className="App">
 
       <Header />
-
       <main>
+
+
         <section id='inicio' className={styles.inicio}>
           <div className={styles.codeH1}>
             <p className={styles.title}>Hola,</p>
             <p className={styles.title}>Soy <span>Alberto</span>,</p>
-            <p className={styles.title}>Desarrollador Web.</p>	
+            <p className={styles.title}>Desarrollador Web.</p>
           </div>
-            <p className={styles.codeP}>Front-End y Back-End Developer</p>
-            
-            <div className={styles.buttons}>
-              <button className='buttonPrimary'>Contactame</button>
-              <button className='btnSecun'>CV</button>
-            </div>
+          <p className={styles.codePGray}>Front-End y Back-End Developer</p>
+
+          <div className={styles.buttons}>
+            <button className='buttonPrimary'>Contactame</button>
+            <button className='btnSecun'>CV</button>
+          </div>
         </section>
 
+        <img src={svg} alt="" className={styles.svg}/>
+
         <section id='acerca_de_mi' className={styles.about}>
-          <div className={styles.heart}></div>
+          <div>
+            <h2 className={styles.codeH2}>Acerca de mí</h2>
+            <p className={styles.codeP}>
+              Soy un desarrollador frontend apasionado por la creación de
+              experiencias web intuitivas y atractivas. Mientras continúo
+              desarrollando mis habilidades en el frontend, estoy
+              emocionado de explorar nuevas oportunidades en el backend
+              para poder ofrecer soluciones web más completas y efectivas.
+            </p>
+          </div>
+          <div className={styles.image}>
+            <img src={image} alt="me" />
+          </div>
         </section>
 
         <section id='proyectos'>
-          
         </section>
 
         <section id='habilidades'>
@@ -44,7 +59,7 @@ function App() {
           <h2>Contacto</h2>
         </section>
       </main>
-      
+
     </div>
   )
 }
