788764c4c58a286a63ac02e9beef8ca642779b07
AlbertoLcm <alberto.cm4007@gmail.com>
2023-04-30 10:15:42 -0600

    form, skills, responsive added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
-import styles from '../styles/Header.module.css'
-import image from '../assets/lcm.svg'
-import { useState } from 'react';
-import { Fragment } from 'react';
+import { NavLink } from "react-router-dom";
+import { useState, Fragment } from "react";
+import image from "../assets/lcm.svg";
+import styles from "../styles/Header.module.css";
 
 const HeaderBurger = ({ action }) => {
   return (
     <header className={styles.burger__container}>
       <div onClick={() => action(false)} className={styles.burger}>
-        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x" width="40" height="40" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#f3f3f3" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-x"
+          width="40"
+          height="40"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="#f3f3f3"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <line x1="18" y1="6" x2="6" y2="18" />
           <line x1="6" y1="6" x2="18" y2="18" />
@@ -19,44 +30,112 @@ const HeaderBurger = ({ action }) => {
       <nav className={styles.nav__header}>
         <ul>
           <li>
-            <a href="#inicio">Inicio</a>
+            <NavLink to="/">Inicio</NavLink>
           </li>
           <li>
-            <a href="#acerca_de_mi">Acerca de mí</a>
+            <NavLink to="/about">Acerca de mí</NavLink>
           </li>
           <li>
-            <a href="#proyectos">Proyectos</a>
+            <NavLink to="/projects">Portafolio</NavLink>
           </li>
           <li>
-            <a href="#habilidades">Habilidades</a>
+            <NavLink to="/skills">Habilidades</NavLink>
           </li>
           <li>
-            <a href="#testimonios">Testimonios</a>
-          </li>
-          <li>
-            <a href="#contacto">Contacto</a>
+            <NavLink to="/testimonials">Contacto</NavLink>
           </li>
         </ul>
       </nav>
+      <div className={styles.icons}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="#f3f3f3"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+          <path d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="#f3f3f3"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="#f3f3f3"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <rect x="4" y="4" width="16" height="16" rx="4" />
+          <circle cx="12" cy="12" r="3" />
+          <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="#f3f3f3"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+        </svg>
+      </div>
     </header>
-  )
-}
+  );
+};
 
 const ButtonBurger = ({ action }) => {
   return (
     <div className={styles.burger} onClick={() => action(true)}>
-      <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" width="40" height="40" viewBox="0 0 24 24" strokeWidth="2" stroke="#f3f3f3" fill="none" strokeLinecap="round" strokeLinejoin="round">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-menu-2"
+        width="40"
+        height="40"
+        viewBox="0 0 24 24"
+        strokeWidth="2"
+        stroke="#f3f3f3"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <line x1="4" y1="6" x2="20" y2="6" />
         <line x1="4" y1="12" x2="20" y2="12" />
         <line x1="4" y1="18" x2="20" y2="18" />
       </svg>
     </div>
-  )
-}
+  );
+};
 
 export default function Header() {
-
   const [burger, setBurger] = useState(false);
 
   const showBurger = (boolean) => setBurger(boolean);
@@ -70,29 +149,96 @@ export default function Header() {
         <nav className={styles.nav__header}>
           <ul>
             <li>
-              <a href="#inicio">Inicio</a>
+              <NavLink to="/">Inicio</NavLink>
             </li>
             <li>
-              <a href="#acerca_de_mi">Acerca de mí</a>
+              <NavLink to="/about">Acerca de mí</NavLink>
             </li>
             <li>
-              <a href="#proyectos">Proyectos</a>
+              <NavLink to="/projects">Portafolio</NavLink>
             </li>
             <li>
-              <a href="#habilidades">Habilidades</a>
+              <NavLink to="/skills">Habilidades</NavLink>
             </li>
             <li>
-              <a href="#testimonios">Testimonios</a>
-            </li>
-            <li>
-              <a href="#contacto">Contacto</a>
+              <NavLink to="/testimonials">Contacto</NavLink>
             </li>
           </ul>
         </nav>
+        <div className={styles.icons}>
+          <a href="" target="_blank">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="#f3f3f3"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+              <path d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
+            </svg>
+          </a>
+
+          <a href="https://www.facebook.com/profile.php?id=100009566345222" target="_blank">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="#f3f3f3"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+            </svg>
+          </a>
+          <a href="" target="_blank">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="#f3f3f3"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <rect x="4" y="4" width="16" height="16" rx="4" />
+              <circle cx="12" cy="12" r="3" />
+              <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+            </svg>
+          </a>
+          <a href="https://github.com/AlbertoLcm" target="_blank">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="#f3f3f3"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+            </svg>
+          </a>
+        </div>
       </header>
 
       <ButtonBurger action={showBurger} />
       {burger && <HeaderBurger action={showBurger} />}
     </Fragment>
-  )
+  );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
-import { H1, H2, P, Main, Body, Section, Card } from './Elements'
-import { ButtonPrimary, ButtonSecundary } from './Buttons'
-import styles from '../styles/Home.module.css'
-import Typewriter from 'typewriter-effect'
-import me from '../assets/images/me.svg'
-import Header from './Header'
-import icons from '../assets/Icons.svg'
+import { H1, H2, P, Main, Body, Section, Card, Other } from "./Elements";
+import { ButtonPrimary, ButtonSecundary } from "./Buttons";
+import styles from "../styles/Home.module.css";
+import Typewriter from "typewriter-effect";
+import me from "../assets/images/me.svg";
+import Header from "./Header";
+import icons from "../assets/Icons.svg";
+import { TextField } from "@mui/material";
+import marker from "../assets/marker.svg";
 
 function Home() {
   return (
@@ -16,31 +18,32 @@ function Home() {
         <Body>
           <section className={styles.home}>
             <H1>
-              Hola, <br /> Soy <span className={styles.tittle__color}>Alberto</span>,
+              Hola, <br /> Soy{" "}
+              <span className={styles.tittle__color}>Alberto</span>,
               <Typewriter
                 options={{
-                  strings: ['Desarrollador Web.', 'Web developer.'],
+                  strings: ["Desarrollador Web.", "Web developer."],
                   autoStart: true,
                   loop: true,
                 }}
               />
             </H1>
-            <P gray={true}>Front-End y Back-End Developer</P>
+            <P gray={true}>Front-End y proximo Back-End Developer.</P>
             <div className={styles.buttons__container}>
               <ButtonPrimary> Contactame </ButtonPrimary>
               <ButtonSecundary> CV </ButtonSecundary>
             </div>
           </section>
 
-          <section id='acerca_de_mi' className={styles.about}>
+          <section id="acerca_de_mi" className={styles.about}>
             <div className={styles.about__description}>
               <H2>Acerca de mí.</H2>
               <P>
-                Soy un desarrollador frontend apasionado por la creación de
-                experiencias web intuitivas y atractivas. Mientras continúo
-                desarrollando mis habilidades en el frontend, estoy
-                emocionado de explorar nuevas oportunidades en el backend
-                para poder ofrecer soluciones web más completas y efectivas.
+                En mi enfoque de desarrollo, siempre trato de encontrar un
+                equilibrio entre el diseño y la funcionalidad. Me aseguro de que
+                cada sitio web o aplicación que construyo tenga un diseño
+                atractivo y una experiencia de usuario intuitiva, mientras que
+                también sea eficiente y esté bien estructurado en el backend.
               </P>
             </div>
 
@@ -51,8 +54,8 @@ function Home() {
             </div>
           </section>
 
-          <section id='projects' className={styles.projects}>
-            <H2>Proyectos.</H2>
+          <section id="projects" className={styles.projects}>
+            <H2>Portafolio.</H2>
             <Section>
               <section className={styles.cards__container}>
                 <Card green={true}>Hola</Card>
@@ -62,26 +65,98 @@ function Home() {
             </Section>
           </section>
 
-          <section id='skills' className={styles.skills}>
+          <section id="skills" className={styles.skills}>
             <section className={styles.skills__content}>
               <div>
-                <H2>Habilidades</H2>
+                <H2>Habilidades y Experiencia.</H2>
+                <P>
+                  A los 20 años de edad, comencé a trabajar de manera
+                  independiente en proyectos de desarrollo web. Durante el
+                  último año de mi carrera como ingeniero en sistemas, tuve la
+                  oportunidad de trabajar en una empresa desarrollando
+                  aplicaciones web y móviles tanto en frontend como en backend.
+                  <br /> <br />
+                  Este trabajo me permitió aplicar mis conocimientos y
+                  habilidades en un entorno real y adquirir experiencia en el
+                  desarrollo de aplicaciones completas y de alta calidad.
+                  <br /> <br />
+                  Desde entonces, he seguido desarrollando mi carrera
+                  profesional en el área de la tecnología, con una gran pasión
+                  por el desarrollo de software y el aprendizaje constante de
+                  nuevas tecnologías y herramientas.
+                </P>
               </div>
               <div>
-                Front-End
-                <progress max="100" value="70"> 70% </progress>
-                Back End
-                <progress max="100" value="60"> 60% </progress>
-                React Js
-                <progress max="100" value="70"> 70% </progress>
-                Node Js
-                <progress max="100" value="50"> 50% </progress>
+                <div className={styles.progress}>
+                  <span>Front-End</span>
+                  <progress max="100" value="80">
+                    80%
+                  </progress>
+                </div>
+                <div className={styles.progress}>
+                  <span>Back End</span>
+                  <progress max="100" value="60">
+                    60%
+                  </progress>
+                </div>
+                <div className={styles.progress}>
+                  <span>React Js</span>
+                  <progress max="100" value="70">
+                    70%
+                  </progress>
+                </div>
+                <div className={styles.progress}>
+                  <span>Express</span>
+                  <progress max="100" value="60">
+                    60%
+                  </progress>
+                </div>
               </div>
             </section>
           </section>
 
-          <section id='contacto' className={styles.contact}>
-            <H2>Contacto</H2>
+          <section id="contacto" className={styles.contact}>
+            <section className={styles.contactContent}>
+              <div>
+                <H2>Contactame.</H2>
+                <Other name={"form"}>
+                  <form className={styles.form}>
+                    <section>
+                      <TextField
+                        fullWidth
+                        id="name"
+                        label="Nombre"
+                        variant="outlined"
+                      />
+                      <TextField
+                        fullWidth
+                        id="email"
+                        label="Correo Electrónico"
+                        variant="outlined"
+                      />
+                    </section>
+                    <TextField
+                      fullWidth
+                      id="asunto"
+                      label="Asunto"
+                      variant="outlined"
+                    />
+                    <TextField
+                      fullWidth
+                      id="mensaje"
+                      label="Mensaje"
+                      variant="outlined"
+                      multiline
+                      rows={6}
+                    />
+                    <ButtonPrimary>Enviar</ButtonPrimary>
+                  </form>
+                </Other>
+              </div>
+              <section className={styles.map}>
+                <img src={marker} />
+              </section>
+            </section>
           </section>
         </Body>
       </Main>
